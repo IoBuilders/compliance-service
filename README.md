751b85393ca5b0d47265e6dd0c908506b7cfb49c
Daniel Lehrner <daniel@io.builders>
2019-05-13 16:38:42 +0200

    Initial project setup

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# compliance-service
-This is the standard implementation for EIP-2009 (Compliance Service)
+# Compliant Service
+
+This is the work in progress implementation of [EIP-2009 Compliant Service](https://github.com/ethereum/EIPs/pull/2009/files). This implementation will change over time with the standard and is not stable at the moment.
+
+Feedback is appreciated and can given at [the discussion of the EIP](https://github.com/ethereum/EIPs/issues/2022).
---
+++ contracts/ICompliantService.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.5.0;
+
+interface IHoldable {
+    function checkTransferAllowed(bytes32 tokenId, address from, address to, uint256 value) external view returns (byte);
+    function checkTransferFromAllowed(bytes32 tokenId, address sender, address from, address to, uint256 value) external view returns (byte);
+    function checkMintAllowed(bytes32 tokenId, address to, uint256 value) external view returns (byte);
+    function checkBurnAllowed(bytes32 tokenId, address from, uint256 value) external view returns (byte);
+
+    function updateTransferAccumulated(bytes32 tokenId, address from, address to, uint256 value) external;
+    function updateMintAccumulated(bytes32 tokenId, address to, uint256 value) external;
+    function updateBurnAccumulated(bytes32 tokenId, address from, uint256 value) external;
+
+    function addToken(bytes32 tokenId, address token) external;
+    function replaceToken(bytes32 tokenId, address token) external;
+    function removeToken(bytes32 tokenId) external;
+    function isToken(address token) external view returns (bool);
+    function getTokenId(address token) external view returns (bytes32);
+
+    function authorizeAccumulatedOperator(address operator) external returns (bool);
+    function revokeAccumulatedOperator(address operator) external returns (bool);
+    function isAccumulatedOperatorFor(address operator, bytes32 tokenId) external view returns (bool);
+
+    event TokenAdded(bytes32 indexed tokenId, address indexed token);
+    event TokenReplaced(bytes32 indexed tokenId, address indexed previousAddress, address indexed newAddress);
+    event TokenRemoved(bytes32 indexed tokenId);
+    event AuthorizedAccumulatedOperator(address indexed operator, bytes32 indexed tokenId);
+    event RevokedAccumulatedOperator(address indexed operator, bytes32 indexed tokenId);
+}
